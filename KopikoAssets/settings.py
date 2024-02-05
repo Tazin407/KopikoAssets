3d86c9c47f61239b889ea1ad10b819afd605e288
tazin407 <tazintasnim34@gmail.com>
2024-02-05 09:47:44 +0600

    added CSRF trusted origin in settings

--- KopikoAssets/settings.py
+++ KopikoAssets/settings.py
@@ -35,6 +35,9 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://kopiko-assets2.onrender.com','https://*.127.0.0.1']
+
+
 
 
 # Application definition
